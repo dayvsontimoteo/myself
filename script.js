19432cedf7b55794a04f352afa215bfdba3cd37b
Dayvson Timoteo <dayvsonn.ti@gmail.com>
2023-09-21 18:39:10 -0300

    Firs commit

---
+++ script.js
@@ -0,0 +1,14 @@
+function toggleMode() {
+  const html = document.documentElement
+  const img = document.querySelector("#profile img")
+
+  if (html.classList.contains("light")) {
+    html.classList.remove("light")
+    html.classList.add("dark")
+    img.setAttribute("src", "./assets/avatar.png")
+  } else {
+    html.classList.remove("dark")
+    html.classList.add("light")
+    img.setAttribute("src", "./assets/avatar-light.png")
+  }
+}
